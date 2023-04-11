5edb8cb3505d42a2a949a9fbd2c43eb5889ed4f9
Wojciech Lidwin <59863785+Halal37@users.noreply.github.com>
2023-04-11 23:20:03 +0200

    Added project files

---
+++ Wrokshop/Entities/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Wrokshop.Entities
+{
+    public class ApplicationDBContext: DbContext
+    {
+        public DbSet<CarWorkshopEntity> CarWorkshop { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=Workshop;Trusted_Connection=True;");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ Wrokshop/Entities/CarWorkshopEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace Wrokshop.Entities
+{
+    public class CarWorkshopEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string? Description { get; set; }
+
+        public string Address { get; set; } = default!;
+        public string? PhoneNumber { get; set; }
+    }
+}
